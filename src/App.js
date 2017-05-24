52fd020cf8e82a5681fdbff402c79f8dd4ed64b0
HSU Rynki <Gernischt@gmail.com>
2017-05-24 17:16:44 +0800

    init repo

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      poetry: null
+    }
+  }
+
+  scrollToRight() {
+    let content = window.$(ReactDOM.findDOMNode(this.refs.content));
+    setTimeout(() => {window.scrollTo(content.width(), 0)}, 10)
+  }
+
+  openCommit() {
+    window.$('.mask').fadeIn();
+    window.$('.footer').animate({opacity: 1}, 'fast');
+    window.$('.footer').slideDown();
+  }
+
+  closeCommit() {
+    window.$('.mask').fadeOut();
+    window.$('.footer').slideUp();
+  }
+
+  start() {
+    window.$('body').fadeIn(800);
+    this.scrollToRight();
+    window.$('.switch').on('click', this.openCommit);
+    window.$('.mask').on('click', this.closeCommit);
+    window.$('.close-commit').on('click', this.closeCommit);
+  }
+
+  componentDidMount() {
+    window.$.ajax({
+      headers: {
+        'x-access-token': 'p.rynki.com'
+      },
+      url: "http://192.168.0.103:4000/todays/poetry",
+      dataType: 'json',
+      type: 'GET',
+      cache: false,
+      success: (data) => {
+        // console.log(data);
+        this.setState({
+          poetry: data
+        });
+      },
+      error: (xhr, text, errorThrown) => {
+        // window.alert(xhr.readyState);
+        console.log(xhr.readyState);
+      }
+    });
+  }
+
+  componentDidUpdate() {
+    this.start();
+  }
+
+  render() {
+    let poetry = this.state.poetry;
+    let content = null;
+    if (poetry !== null) {
+      let link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = `http://poetry.shy07.com/assets/css/${poetry.hash}/SentyTang.css`;
+      link.type = "text/css";
+      document.head.appendChild(link);
+      document.title = `${poetry.title} - ${poetry.author}`;
+      let commits = null;
+      if (poetry.commit.length > 0) {
+        let commit = poetry.commit.map((c, i) => (<small key={i}>{c}<br/></small>));
+        commits = (
+          <div>
+            <div className="mask"></div>
+            <div className="switch">
+              <span><small>注</small></span>
+            </div>
+            <div className="footer">
+              <div className="footer-nav">注释 <span className="close-commit"><small>折叠</small></span></div>
+              <p className="commit">
+                {commit}
+              </p>
+            </div>
+          </div>
+        )
+      }
+      content = (
+        <div>
+          <div ref='content' className="content">
+            <p className="poetry">{poetry.content}</p>
+            <div className='title'>{poetry.title}
+              <div className='author'>{poetry.author}<span>詩</span></div>
+            </div>
+          </div>
+          {commits}
+        </div>
+      )
+    }
+    return content;
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+import jQuery from 'jquery';
+window.jQuery = window.$ = jQuery;
+
+ReactDOM.render(<App />, document.getElementById('root'));
